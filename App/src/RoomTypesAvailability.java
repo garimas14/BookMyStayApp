465673132a71f4d53441fb2e1934f4134d52542f
garimas14 <gs2432@srmist.edu.in>
2026-04-03 18:11:21 +0530

    Garima Added UC2

---
+++ App/src/RoomTypesAvailability.java
@@ -0,0 +1,77 @@
+abstract class Room {
+    private String roomType;
+    private int beds;
+    private int size;
+    private double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println(roomType + ":");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 750, 5000.0);
+    }
+}
+
+public class RoomTypesAvailability {
+    public static void main(String[] args) {
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
